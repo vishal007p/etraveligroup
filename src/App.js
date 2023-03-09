b4404e2adc8f31c375bcfb55bdcc2230460e61c4
hari shankar <harishankar@newtowntek.com>
2023-03-08 20:10:25 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Layout from "./Container/Layout";
 const App = () => {
   return (
     <div className="App">
+      <h1>Hello User</h1>
       <Layout />
     </div>
   );
-}
+};
 
 export default App;
