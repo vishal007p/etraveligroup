ce05e964ddbcb6a179f3988a12da2ae9e2f2cf31
hari shankar <harishankar@newtowntek.com>
2023-03-11 19:23:09 -0800

    js function

---
+++ src/classes/Javascript/array/allFunction.js
@@ -0,0 +1,13 @@
+// map   - retrun same array length
+// find  - retun single object based on condition
+// foreach
+// forin
+// forof
+// slice
+// splice
+// reduce
+// indexOf
+// sort  --
+// push
+// pop
+// filter  --  retunrn array acording condition
